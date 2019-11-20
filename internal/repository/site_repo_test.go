fc8e9822579cab137a8e48d992c2d30da9cd3d4e
Rojali Budi Permadi <rojali.budi@bukalapak.com>
2019-11-20 17:31:32 +0700

    update site_repo unit test

--- internal/repository/site_repo_test.go
+++ internal/repository/site_repo_test.go
@@ -4,40 +4,42 @@ import (
 	"testing"
 
 	"github.com/rbpermadi/hooq/internal/repository"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestSiteRepo_All(t *testing.T) {
 	siteRepo := repository.SiteRepo{}
 
-	sitesNull := siteRepo.All()
-
-	site := repository.Site{
-		ID:     1,
-		Link:   "http://google.com",
-		Status: "healthy",
-	}
-
-	site2 := repository.Site{
-		ID:     2,
-		Link:   "http://github.com",
-		Status: "unhealthy",
-	}
-
-	siteRepo.Create(site)
-	siteRepo.Create(site2)
-
-	sites := siteRepo.All()
-	lastSite := sites[len(sites)-1]
+	// test when data null
+	t.Run("data null", func(t *testing.T) {
+		sitesNull := siteRepo.All()
+		if len(sitesNull) != 0 {
+			t.Errorf("All() failed, expected %v, got %v", 0, len(sitesNull))
+		}
+	})
 
 	// test when data not null
 	t.Run("success", func(t *testing.T) {
-		assert.Equal(t, site2.Link, lastSite.Link)
-	})
+		site := repository.Site{
+			ID:     1,
+			Link:   "http://google.com",
+			Status: "healthy",
+		}
 
-	// test when data null
-	t.Run("data null", func(t *testing.T) {
-		assert.True(t, len(sitesNull) == 0)
+		site2 := repository.Site{
+			ID:     2,
+			Link:   "http://github.com",
+			Status: "unhealthy",
+		}
+
+		siteRepo.Create(site)
+		siteRepo.Create(site2)
+
+		sites := siteRepo.All()
+		lastSite := sites[len(sites)-1]
+
+		if site2.Link != lastSite.Link {
+			t.Errorf("All() failed, expected %v, got %v", site2.Link, lastSite.Link)
+		}
 	})
 }
 
@@ -51,7 +53,9 @@ func TestSiteRepo_Create(t *testing.T) {
 
 	site := siteRepo.Create(newSite)
 
-	assert.Equal(t, newSite.Link, site.Link)
+	if newSite.Link != site.Link {
+		t.Errorf("Create() failed, expected %v, got %v", newSite.Link, site.Link)
+	}
 }
 
 func TestSiteRepo_Update(t *testing.T) {
@@ -70,15 +74,25 @@ func TestSiteRepo_Update(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		updateResult, err := siteRepo.Update(0, *site)
 
-		assert.Nil(t, err)
-		assert.Equal(t, site.Status, updateResult.Status)
+		if err != nil {
+			t.Errorf("Update() failed, expected %v, got %v", "nil", err)
+		}
+
+		if site.Status != updateResult.Status {
+			t.Errorf("Update() failed, expected %v, got %v", site.Status, updateResult.Status)
+		}
 	})
 
 	// test when data null
-	t.Run("data null", func(t *testing.T) {
+	t.Run("data not found", func(t *testing.T) {
 		_, err := siteRepo.Update(10, *site)
 
-		assert.NotNil(t, err)
-		assert.Equal(t, "Site not found", err.Error())
+		if err == nil {
+			t.Errorf("Update() failed, expected %v, got %v", "not nil", err)
+		}
+
+		if err.Error() != "Site not found" {
+			t.Errorf("Update() failed, expected %v, got %v", "Site not found", err.Error())
+		}
 	})
 }
